6f14c76a2e6b5cd26b2272d14aafe3ab19d86401
ryan <ryanjgecelter@gmail.com>
2022-10-26 19:04:30 +0300

    edited name of minyan_express.sql

---
+++ minyan_express.sql
@@ -0,0 +1,18 @@
+\echo 'Delete and recreate minaynExpress db?'
+\prompt 'Return for yes or control-C to cancel > ' foo
+
+DROP DATABASE minyan_express;
+CREATE DATABASE minyan_express;
+\connect minyan_express
+
+\i minyan_express_schema.sql
+-- \i minyan_express_seed.sql
+
+\echo 'Delete and recreate jobly_test db?'
+\prompt 'Return for yes or control-C to cancel > ' foo
+
+DROP DATABASE minyan_express_test;
+CREATE DATABASE minyan_express_test;
+\connect minyan_express_test
+
+\i minyan_express_schema.sql
