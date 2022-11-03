ae6a847a8eefd30e2d99d5651e2590abce037b4b
ryan <ryanjgecelter@gmail.com>
2022-11-03 10:41:46 +0200

    added events and reservations join tables

--- minyan_express_schema.sql
+++ minyan_express_schema.sql
@@ -9,12 +9,29 @@ CREATE TABLE users (
 
 CREATE TABLE locations (
     id SERIAL PRIMARY KEY,
-    nick_name VARCHAR(25),
+    nick_name TEXT,
     street TEXT NOT NULL, 
     number INTEGER NOT NULL,
     city TEXT NOT NULL, 
     zip INTEGER,
-    -- coordinates 
+    -- coordinates? 
     is_public BOOLEAN NOT NULL DEFAULT true,
     location_capacity_limit INTEGER DEFAULT 15 
 );
+
+CREATE TABLE events (
+    id SERIAL PRIMARY KEY,
+    start_time TIME,
+    end_time TIME,
+    location_id INTEGER NOT NULL
+        REFERENCES locations ON DELETE CASCADE,
+    current_capacity INTEGER NOT NULL DEFAULT 0,
+    max_capacity INTEGER; --nice to have for later 
+)
+
+CREATE TABLE reservations (
+    user_id INTEGER NOT NULL REFERENCES users 
+        ON DELETE CASCADE,
+    event_id INTEGER NOT NULL REFERENCES events 
+        ON DELETE CASCADE;
+)
