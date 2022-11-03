123a22c36c5233ae8554a36679c2c0be522325ff
ryan <ryanjgecelter@gmail.com>
2022-11-03 11:15:28 +0200

    typo

--- minyan_express_seed.sql
+++ minyan_express_seed.sql
@@ -6,4 +6,5 @@ VALUES ('testuser', '$2y$04$JI7wHgnq4qEAboSFThgLCeqt2vaNZZbGjOejCHUDnPaPZKiMyean
 -- needs country, make street number optional, add column for notes about location, maybe also room? floor? details
 INSERT INTO locations (nick_name, street, number, city, zip, is_public, location_capacity_limit) 
 VALUES ('airport synagogue', 'Ben Gurion Airport', 1, 'Tel Aviv', 7015001, 'true', 35), 
-('kinar galilee hotel synagogue', 'צפון מזרח הכנרת רמות,',1, 'Ramot', 12490, 'true', 25)
+('kinar galilee hotel synagogue', 'צפון מזרח הכנרת רמות,',1, 'Ramot', 12490, 'true', 25);
+
