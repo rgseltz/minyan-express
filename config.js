c5942fbf41b9037c2f4ccda5193ffe3775144aeb
ryan <ryanjgecelter@gmail.com>
2022-11-03 15:58:51 +0200

    add feature SECRET_KEY to config.js and exported to token.js

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 let PORT = 3001 || process.env.PORT;
 
+let SECRET_KEY = process.SECRET_KEY || 'davening-dev32'
+
 function getDatabaseUri() {
     return (process.env.NODE_ENV === "test") ? "minyan_express_test" : "minyan_express" || DATABASE_URL;
 }
@@ -7,4 +9,4 @@ function getDatabaseUri() {
 /*bcrypt work factor algorithm slows decryption - want to be slow for development and production and fast for testing **/
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
-module.exports = { getDatabaseUri, PORT, BCRYPT_WORK_FACTOR }
+module.exports = { getDatabaseUri, PORT, BCRYPT_WORK_FACTOR, SECRET_KEY }
