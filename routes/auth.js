b9bc3b484febd8187e98258319e88cc76554f8e8
ryan <ryanjgecelter@gmail.com>
2022-11-03 12:49:04 +0200

    add register route to auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const expressError = require('../expressError');
-const { createToken } = require('../helpers/tokens');
+const jsonschema = require('jsonschema'); //for validation of data to api
+const { createToken } = require('../helpers/tokens'); //to create token once validated
+const userRegisterSchema = require('../json_schemas/userRegister.json');
+const User = require('../models/user');
 const router = new express.Router();
 
 /* POST: auth/register 
-    creates user token to be used to authenticate further user requests 
+    RETURNS a new user token to be used to authenticate further user requests 
     req.body must include {username, firstName, lastName, password, email}**/
 
+router.post('/register', async (req, res, next) => {
+    try {
+        const validator = jsonschema.validate(req.body, userRegisterSchema);
+        if (!validator.valid) {
+            let errorList = validator.errors.map(e => e.stack);
+            throw new expressError(errorList, 400);
+        };
+        console.log('valid request');
+
+        const newUser = new User({ ...req.body, isAdmin: false });
+        const token = createToken(newUser);
+        return res.json({ token });
+    } catch (err) {
+        return next(err);
+    }
+})
 
 
 module.exports = router;
