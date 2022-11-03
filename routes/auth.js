97cc57a08c7c34fc6825c116feb2b373c25bd0d9
ryan <ryanjgecelter@gmail.com>
2022-11-03 12:35:41 +0200

    added deps jsonschema and setting up auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const expressError = require('../expressError');
+const { createToken } = require('../helpers/tokens');
 const router = new express.Router();
+
+/* POST: auth/register 
+    creates user token to be used to authenticate further user requests 
+    req.body must include {username, firstName, lastName, password, email}**/
+
+
 
 module.exports = router;
