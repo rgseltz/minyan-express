445409924788b2ad1f1ff78c1812b65472ca8e48
ryan <ryanjgecelter@gmail.com>
2022-11-07 10:06:54 +0200

    updated Event class to include serviceType

--- models/event.js
+++ models/event.js
@@ -9,7 +9,7 @@ class Event {
      *  accepts data {start time and end time} and    
      */
     static async create(data, locationId) {
-        let { startTime, endTime, currentCapacity } = data;
+        let { startTime, endTime, serviceType, currentCapacity } = data;
         currentCapacity++;
         const checkLocation = await db.query(
             `SELECT id, nick_name FROM locations WHERE id = $1`, [locationId]
@@ -17,9 +17,11 @@ class Event {
         if (!checkLocation.rows[0]) throw new expressError('Invalid location', 404);
         console.log(currentCapacity);
         console.log(locationId);
+
+        //PROBLEM: Overlapping times - how do I convert times and ensure that no two events occur at the same location in same time block?
         const newEvent = await db.query(
-            `INSERT INTO events (start_time, end_time, location_id, current_capacity) 
-             values ($1, $2, $3, $4)`, [startTime, endTime, locationId, currentCapacity]
+            `INSERT INTO events (start_time, end_time, service_type, location_id, current_capacity) 
+             values ($1, $2, $3, $4, $5) RETURNING id AS "locationId"`, [startTime, endTime, serviceType, locationId, currentCapacity]
         );
         console.log(newEvent.rows[0]);
         return newEvent.rows[0];
