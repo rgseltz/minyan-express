0185ee72f135f4b8b6522aca56eabd5a5ed7a169
ryan <ryanjgecelter@gmail.com>
2022-11-13 16:13:19 +0200

    added dep cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const db = require('./db');
 const expressError = require('./expressError')
 const app = express();
+const cors = require('cors');
 
 const authRoutes = require('./routes/auth')
 const userRoutes = require('./routes/users');
@@ -13,6 +14,7 @@ const { authenticateJWT } = require('./middleware/auth');
 
 app.use(authenticateJWT);
 
+app.use(cors());
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
